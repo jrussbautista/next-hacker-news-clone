442eedc3343eced31cc145dee2aaeb92fb1d16ba
jrussumbrella <jrussclay@gmail.com>
2019-08-12 17:25:05 +0800

    add next analytics

--- pages/_document.js
+++ pages/_document.js
@@ -1,10 +1,25 @@
 import Document, { Head, Main, NextScript } from "next/document";
+import { GA_TRACKING_ID } from '../analytics/gtag';
 
 export default class MyDocument extends Document {
   render() {
     return (
       <html lang="en-US">
         <Head>
+        <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}');
+          `
+            }}
+          />          
           <link rel="manifest" href="/static/manifest.json" />
           <meta name="mobile-web-app-capable" content="yes" />
           <meta name="apple-mobile-web-app-capable" content="yes" />
